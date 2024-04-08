5efe515bb6210ac66841842f4f6e095b15d56550
Hajiyev2006 <rzah20536@gmail.com>
2024-04-08 22:58:23 +0400

    first commit

---
+++ app.js
@@ -0,0 +1,29 @@
+// #######################################################################TASK 1##################################################################
+// const items = [
+    //     { name: 'Mehsul 1', price: 10 },
+    //     { name: 'Mehsul 2', price: 5 },
+    //     { name: 'Mehsul 3', price: 20 },
+    //     { name: 'Mehsul 4', price: 15 },
+    //     { name: 'Mehsul 5', price: 8 }
+    // ];
+    // function filterItems(items) {
+        //     const prices = items.sort((a, b) => a.price - b.price);
+        //     return prices;
+        // }
+        // const prices = filterItems(items);
+        // console.log("Ucuzdan bahaya mehsullarin sirasi: ");
+        // prices.forEach(item => {
+            //     console.log(`${item.name} - ${item.price}$`);
+            // });
+            
+// #######################################################################TASK 2##################################################################
+// const randomNumbers = [];
+// for (let i = 0; i < 5; i++) {
+//     randomNumbers.push(Math.floor(Math.random() * 100) + 1); 
+// }
+// let cem = 0;
+// for (let i = 0; i < randomNumbers.length; i++) {
+//     cem += randomNumbers[i];
+// }
+// console.log("Random ededler: ", randomNumbers);
+// console.log("Random ededlerin cemi: ", cem);
